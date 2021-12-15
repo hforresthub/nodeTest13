23ca7d62cf023bd2fbcbc269bbef36151532a0ac
Hal <hgrforrest@gmail.com>
2021-12-15 15:49:36 -0500

    testing os

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
+// path tests 
+
 const path = require('path')
+const os = require('os')
 
 const filename1 = './data1.json'
 const filePathObj = path.parse(filename1)
@@ -16,3 +19,21 @@ console.log(filePathRel);
 const filePathNormal = path.normalize(filename1)
 
 console.log(filePathNormal);
+
+// os tests 
+
+console.log(os.arch());
+console.log(os.cpus());
+console.log(os.endianness());
+console.log(os.freemem());
+console.log(os.homedir());
+console.log(os.hostname());
+console.log(os.loadavg());
+console.log(os.networkInterfaces());
+console.log(os.platform());
+console.log(os.release());
+console.log(os.tmpdir());
+console.log(os.totalmem());
+console.log(os.type());
+console.log(os.uptime());
+console.log(os.userInfo());
